81c7b0e99929d28493907afc983dde483974b1e7
Yaron Schwimmer <yaron@perimeterx.com>
2019-03-28 17:51:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![image](https://s.perimeterx.net/logo.png)
+
+# [PerimeterX](http://www.perimeterx.com) base eslint configuration
+
+## Motivation
+While [`eslint:recommended`](https://github.com/eslint/eslint/blob/master/conf/eslint-recommended.js) gives a pretty good baseline, there are some cases we want to keep things more strict.  
+Instead of rewriting a new set of rules for each project, we wanted to create a unified set of rules that we can use when we start a new JS project. 
---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": ["error", 4, {"SwitchCase": 1}],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "single", {"allowTemplateLiterals": true, "avoidEscape": true}],
+        "semi": ["error", "always"],
+        "no-useless-escape": ["warn"],
+        "no-undef": ["error"],
+        "no-console": ["warn"],
+        "no-multiple-empty-lines": ["error", {"max": 1}],
+        "no-var": ["error"],
+        "no-eval": ["error"],
+        "no-implied-eval": ["error"],
+        "no-empty": ["error", {"allowEmptyCatch": true}],
+        "curly": ["error", "multi-line", "consistent"],
+        "brace-style": ["error", "1tbs", {"allowSingleLine": true}],
+        "prefer-const": ["error"],
+        "space-before-blocks": ["error", "always"],
+        "comma-spacing": ["error"],
+        "camelcase": ["error", {"properties": "never"}],
+        "eqeqeq": ["error"],
+        "comma-dangle": ["error", "only-multiline"],
+        "space-infix-ops": ["error"],
+        "no-with": ["error"],
+        "no-alert": ["error"],
+        "no-lone-blocks": ["error"],
+        "no-loop-func": ["error"],
+        "no-new-func": ["error"],
+        "no-proto": ["error"],
+        "no-self-compare": ["error"],
+        "no-useless-call": ["error"],
+        "no-useless-concat": ["error"],
+        "no-with": ["error"],
+    }
+};
+
