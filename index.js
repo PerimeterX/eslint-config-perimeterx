fe521bddf895ba0bf14e462bf92e0d3860242c45
Yaron Schwimmer <yaron@perimeterx.com>
2019-10-16 16:50:00 +0300

    added test frameworks to env

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ module.exports = {
     "env": {
         "browser": true,
         "node": true,
-        "es6": true
+        "es6": true,
+        "jest": true,
+        "mocha": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
